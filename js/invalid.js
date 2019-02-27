bde1e86199e86f3ff88a47ac8176dfc331ed8697
SERGEI <mr.kozna@gmail.com>
2019-02-27 20:43:32 +0300

    27/02/19 ПРавки

--- js/invalid.js
+++ js/invalid.js
@@ -1,15 +1,42 @@
-var email = document.querySelector(".email");
-var text = document.getElementsByClassName("name");
-email.addEventListener("input", function (event) {
+var oForm = document.getElementById("wr");
+var email = oForm.querySelector("[type = email]");
+var text = oForm.querySelector("[type = text]");
+
+
+
+
+/*
+oForm.addEventListener("submit", function(evt) { ;
   if (email.validity.typeMismatch) {
     email.setCustomValidity("I expect an e-mail, darling!");
   } else {
     email.setCustomValidity("");
   }
 });
+*/
 
 
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    // email.setCustomValidity("I expect an e-mail, darling!");
+    email.style.backgroundColor = "#EDB5B5"; 
+  } else {
+    // email.setCustomValidity("");
+    email.style.backgroundColor = ""; 
+  }
+});
 
+/*
+text.addEventListener("input", function (event) {
+  if (!text.validity.typeMismatch) {
+    // text.setCustomValidity("I expect an e-mail, darling!");
+    text.style.backgroundColor = "#EDB5B5"; 
+  } else {
+    // email.setCustomValidity("");
+    text.style.backgroundColor = ""; 
+  }
+});
+*/
 
 
 
--- js/slider2.js
+++ js/slider2.js
@@ -13,7 +13,11 @@ linkOne.addEventListener("click", function (evt) {
         evt.preventDefault();
         serviceOne.classList.add("show");
         serviceTwo.classList.remove("show");
-        serviceThree.classList.remove("show");}
+        serviceThree.classList.remove("show");
+        linkOne.classList.add("del-black", "act");
+        linkTwo.classList.remove("war-black", "act");
+        linkThree.classList.remove("cre-black", "act");
+    }
 
 });
 
@@ -24,6 +28,9 @@ linkTwo.addEventListener("click", function (evt) {
         serviceTwo.classList.add("show");
         serviceOne.classList.remove("show");
         serviceThree.classList.remove("show");
+        linkOne.classList.remove("del-black", "act");
+        linkTwo.classList.add("war-black", "act");
+        linkThree.classList.remove("cre-black", "act");
     }
 
 });
@@ -34,6 +41,10 @@ linkThree.addEventListener("click", function (evt) {
         evt.preventDefault();
         serviceThree.classList.add("show");
         serviceOne.classList.remove("show");
-        serviceTwo.classList.remove("show");}
+        serviceTwo.classList.remove("show");
+        linkOne.classList.remove("del-black", "act");
+        linkTwo.classList.remove("war-black", "act");
+        linkThree.classList.add("cre-black", "act");
+    }
 
 });
